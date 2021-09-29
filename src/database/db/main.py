dfcdde7307b8cb5517c59840a8951c517567cb46
egor <shestopalovegor@gmail.com>
2021-09-29 10:14:38 +0300

    [feature] Add endpoints for text manipulation

--- src/database/db/main.py
+++ src/database/db/main.py
@@ -1,16 +1,27 @@
-from typing import Optional
-
+from typing import Optional, List
 
 import sqlalchemy
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from config import DbConfig
 
 
-class Text(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class TextBase(SQLModel):
     raw_text: Optional[str]
     source: Optional[str]
+
+
+class Text(TextBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class TextCreate(TextBase):
+    pass
+
+
+class TextRead(TextBase):
+    id: int
 
 
 engine = create_engine(DbConfig.get_db_url())
@@ -44,11 +55,29 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    create_texts()
 
 
-@app.get("/texts/")
-def read_texts():
+@app.post("/add_text/", response_model=TextRead)
+def add_text(text: TextCreate):
     with Session(engine) as session:
-        texts = session.exec(select(Text)).all()
+        db_text = Text.from_orm(text)
+        session.add(db_text)
+        session.commit()
+        session.refresh(db_text)
+        return db_text
+
+
+@app.get("/texts/", response_model=List[TextRead])
+def texts(offset: int = 0, limit: int = Query(default=100, lte=100)):
+    with Session(engine) as session:
+        texts = session.exec(select(Text).offset(offset).limit(limit)).all()
         return texts
+
+
+@app.get("/text/{text_id}", response_model=TextRead)
+def read_text(text_id: int):
+    with Session(engine) as session:
+        text = session.get(Text, text_id)
+        if not text:
+            raise HTTPException(status_code=404, detail="Text not found")
+        return text
