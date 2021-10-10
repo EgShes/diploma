90af3d5386d5e5d45a0e48bcd1ccf02b0fb73b5f
egor <shestopalovegor@gmail.com>
2021-10-10 20:27:58 +0300

    [feature] Add wiki text scruper

--- src/scrapers/wiki_scrupper/run.py
+++ src/scrapers/wiki_scrupper/run.py
@@ -1,12 +1,76 @@
+import json
+from dataclasses import dataclass
+from itertools import count
+from time import sleep
+from tqdm import tqdm
 import requests
+from argparse import ArgumentParser
+
+import logging
+from src.config import DbConfig
+
+WIKI_RANDOM_PAGE_URL = "https://ru.wikipedia.org/api/rest_v1/page/random/summary"
+TEXT_RETRY_SECONDS = 1
 
 
-def get_random_page() -> str:
-    return "kek"
+class WikiPageNotAvailableError(Exception):
+    pass
+
+
+class TextNotInserted(Exception):
+    pass
+
+
+@dataclass
+class WikiText:
+    title: str
+    text: str
+
+
+def get_random_page() -> WikiText:
+    response = requests.get(WIKI_RANDOM_PAGE_URL)
+    if response.status_code != 200:
+        raise WikiPageNotAvailableError
+    page = json.loads(response.content)
+    return WikiText(page["title"], page["extract"])
+
+
+def post_text(wiki_text: WikiText, url: str):
+    response = requests.post(url, json={"raw_text": wiki_text.text, "source": "wiki"})
+    if response.status_code != 200:
+        raise TextNotInserted
+    return
 
 
 if __name__ == "__main__":
-    url = "https://ru.wikipedia.org/api/rest_v1/page/random/summary"
-    response = requests.get(url)
-    print(response.content)
-    pass
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--max",
+        type=int,
+        default=-1,
+        help="Number of texts to put to database. -1 if no upper limit",
+    )
+    parser.add_argument("--ip", type=str, default="http://0.0.0.0", help="Ip of db api")
+    parser.add_argument("--port", type=int, default=8080, help="Port of db api")
+    args = parser.parse_args()
+
+    url = f"{args.ip}:{args.port}/add_text"
+
+    for i in tqdm(count()):
+        try:
+            text = get_random_page()
+            post_text(text, url)
+            if args.max != -1 and i == args.max:
+                break
+        except WikiPageNotAvailableError:
+            logging.warning(
+                f"Could not get random wiki page. Retrying in {TEXT_RETRY_SECONDS}"
+            )
+            sleep(TEXT_RETRY_SECONDS)
+        except TextNotInserted:
+            logging.warning(
+                f"Could not put text to database. Retrying in {TEXT_RETRY_SECONDS}"
+            )
+            sleep(TEXT_RETRY_SECONDS)
+    logging.info("Finished")
