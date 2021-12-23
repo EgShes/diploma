64dccf1b9ed55ab0a58131b33c26f11219cb6988
egor <shestopalovegor@gmail.com>
2021-12-23 08:48:14 +0300

    [refactor] Refactored text endpoints

--- src/database/routers/text.py
+++ src/database/routers/text.py
@@ -1,29 +1,22 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
-from fastapi.params import Depends
+from fastapi import APIRouter
+from fastapi.params import Depends, Query
+from pydantic import conlist
 from sqlalchemy.orm import Session
 
 from src.database import schemas
-from src.database.crud.text import create_source_text, get_source_text_by_id, get_texts
+from src.database.crud.text import create_source_text, get_source_text_by_ids
 from src.database.routers.utils import get_db
 
 router = APIRouter()
 
 
 @router.post("/add/", response_model=schemas.SourceText)
-def add_text(text: schemas.SourceTextCreate, db: Session = Depends(get_db)):
+def add_text(text: schemas.SourceTextCreate, db: Session = Depends(get_db)) -> schemas.SourceText:
     return create_source_text(db=db, source_text=text)
 
 
-@router.get("/read/", response_model=schemas.SourceText)
-def read_text(text_id: int, db: Session = Depends(get_db)):
-    source_text = get_source_text_by_id(db, id_=text_id)
-    if source_text is None:
-        raise HTTPException(status_code=404, detail=f"Text with id {text_id} not found")
-    return source_text
-
-
-@router.get("/list/", response_model=List[schemas.SourceText])
-def read_texts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_texts(db, skip=skip, limit=limit)
+@router.get("/read/", response_model=List[schemas.SourceText])
+def get_texts(ids: conlist(int, max_items=50) = Query(...), db: Session = Depends(get_db)) -> List[schemas.SourceText]:
+    return get_source_text_by_ids(db, ids)
