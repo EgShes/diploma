68a7a25924adabf3a5cc6c0fa767a11c87f859bb
egor <shestopalovegor@gmail.com>
2022-02-16 22:45:52 +0300

    [fix] Fixed integration tests

--- tests/integration/utils/add_texts.py
+++ tests/integration/utils/add_texts.py
@@ -3,17 +3,19 @@ from pathlib import Path
 import pandas as pd
 import requests
 
-texts_csv_path = Path(__file__).resolve().parents[1] / "test_data.csv"
+url = "http://app:8000/"
+data_path = Path(__file__).resolve().parents[1] / "data"
+
+
+def add_data(url: str, df_path: Path):
+    df = pd.read_csv(df_path)
+
+    for _, row in df.iterrows():
+        response = requests.post(url, json=dict(row))
+        assert response.status_code == 200
 
 
 if __name__ == "__main__":
-
-    texts_df = pd.read_csv(texts_csv_path)
-
-    for _, row in texts_df.iterrows():
-        data = {
-            "text": row["text"],
-            "source": row["source"],
-        }
-        response = requests.post("http://app:8000/text/add/", json=data)
-        assert response.status_code == 200
+    add_data(url + "chat/add/", data_path / "chats.csv")
+    add_data(url + "employee/add/", data_path / "employees.csv")
+    add_data(url + "text/add/", data_path / "texts.csv")
