f7cba6f98552de7fad3b565bc0c18e4624721c73
egor <shestopalovegor@gmail.com>
2022-02-16 21:37:48 +0300

    [feature] Updated wiki scrupper

--- src/scrapers/wiki_scrupper/run.py
+++ src/scrapers/wiki_scrupper/run.py
@@ -3,6 +3,7 @@ import logging
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from itertools import count
+from random import choice
 from time import sleep
 
 import requests
@@ -34,8 +35,10 @@ def get_random_page() -> WikiText:
     return WikiText(page["title"], page["extract"])
 
 
-def post_text(wiki_text: WikiText, url: str):
-    response = requests.post(url, json={"text": wiki_text.text, "source": "wiki"})
+def post_text(wiki_text: WikiText, url: str, chat_id: int, employee_id: int):
+    response = requests.post(
+        url, json={"text": wiki_text.text, "source": "wiki", "employee_id": employee_id, "chat_id": chat_id}
+    )
     if response.status_code != 200:
         raise TextNotInserted
 
@@ -51,6 +54,8 @@ if __name__ == "__main__":
     )
     parser.add_argument("--ip", type=str, default="http://0.0.0.0", help="Ip of db api")
     parser.add_argument("--port", type=int, default=8080, help="Port of db api")
+    parser.add_argument("--chat_ids", nargs="+", type=int, default=(1, 2, 3), help="Ids of chats to add to")
+    parser.add_argument("--employee_ids", nargs="+", type=int, default=(1, 2, 3), help="Ids of employees to add to")
     args = parser.parse_args()
 
     url = f"{args.ip}:{args.port}/text/add/"
@@ -58,7 +63,7 @@ if __name__ == "__main__":
     for i in tqdm(count()):
         try:
             text = get_random_page()
-            post_text(text, url)
+            post_text(text, url, choice(args.chat_ids), choice(args.employee_ids))
             if args.max != -1 and i == args.max:
                 break
         except WikiPageNotAvailableError:
